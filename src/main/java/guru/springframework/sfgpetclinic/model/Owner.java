3788f9abb26761f1cce242cb6a10c0b38391e97d
PatForAll <siekmeier.p@gmail.com>
2021-07-07 22:12:41 +0200

    add petList for Owner; Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,6 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.List;
+
 public class Owner extends Person {
 
+    List<Pet> petList;
 
+    public List<Pet> getPetList() {
+        return petList;
+    }
+
+    public void setPetList(List<Pet> petList) {
+        this.petList = petList;
+    }
 }
