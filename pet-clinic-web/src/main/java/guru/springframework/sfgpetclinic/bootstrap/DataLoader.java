6978c278d6c7d2c7a9df1ccb7b912cffcbb94705
PatForAll <siekmeier.p@gmail.com>
2021-07-22 21:06:31 +0200

    add Pet Types to data loader; closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,25 +16,42 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType petType1 = new PetType();
+        petType1.setName("Dog");
+
+        PetType savedPetType1 = petTypeService.save(petType1);
+
+        PetType petType2 = new PetType();
+        petType1.setName("Cat");
+
+        PetType savedPetType2 = petTypeService.save(petType2);
+
+        System.out.println("Loaded Pet Types...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
-        ownerService.save(owner1);
+        Owner savedOwner1 = ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Johnson");
 
-        ownerService.save(owner2);
+        Owner savedOwner2 = ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
 
@@ -47,5 +68,17 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Loaded Vets...");
+
+        Pet pet1 = new Pet();
+        pet1.setOwner(savedOwner1);
+        pet1.setPetType(savedPetType1);
+
+        petService.save(pet1);
+
+        Pet pet2 = new Pet();
+        pet1.setOwner(savedOwner2);
+        pet1.setPetType(savedPetType2);
+
+        petService.save(pet2);
     }
 }
