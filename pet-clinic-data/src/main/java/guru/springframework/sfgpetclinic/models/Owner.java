43a2d2613dfb339cf8511163c5f49eb59e5d4c6a
PatForAll <siekmeier.p@gmail.com>
2021-10-15 12:30:03 +0200

    make Builder work for extended entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Owner.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.models;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,8 +12,6 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -21,7 +22,15 @@ public class Owner extends Person {
     private String city;
     @Column(name = "telephone")
     private String telephone;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 }
